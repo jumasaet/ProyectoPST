622276e0778488d1d789aaa75eeebe2495028ca4
jumasaet <74018650+jumasaet@users.noreply.github.com>
2022-08-26 13:18:25 -0500

    Funciones de los botones y rediseño de una activity

--- app/src/main/java/com/example/pstproject/MainActivity2.java
+++ app/src/main/java/com/example/pstproject/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -14,5 +15,15 @@ public class MainActivity2 extends AppCompatActivity {
     }
     public void volver(View v){
         this.finish();
+    }
+
+    public void unlock(View v){
+        Toast.makeText(this, "El candado ha sido desbloqueado ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void locate(View v){
+        Toast.makeText(this, "Petición de coordenadas enviada ",
+                Toast.LENGTH_SHORT).show();
     }
 }
